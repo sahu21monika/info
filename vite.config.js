01629aedf46c3eab12f22b1e04a063040c414808
Monika Sahu <sahu21.monika@gmail.com>
2026-09-21 23:35:42 +0200

    Chatboat phase 1

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/info/',
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3001',
+    },
+  },
 })
